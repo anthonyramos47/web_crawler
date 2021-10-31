d9ce3f93e4d35a78661940e7a8cecaf1c174c393
anthonyramos47 <anthonyramos47@gmail.com>
2021-10-30 19:36:06 -0500

    Creation spider class

---
+++ library/spider.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+class spider():
+    """
+        Class Spider to read URL and Interpret HTML
+    """
+    def __init__(self, url : str) -> None:
+        """ Constructor: 
+            Input: url: str -> String with the ulr to read
+        """
+        # Set Url
+        self.url = url 
+        # Set Soup (Reader and Manager of HTML file)
+        self.soup = self.create_soup()
+
+    def create_soup(self):
+        """ Function to create a soup.
+        """
+        # Read Soup if possible, otherwise return None 
+        try :
+            soup = BS(requests.get(self.url).content, "html.parser")
+        except:
+            soup = None
+        return soup
+        
